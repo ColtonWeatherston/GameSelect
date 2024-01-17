e8edda3db62ea94c6caefd578036dacf127e655e
Colton Weatherston <66190795+ColtonWeatherston@users.noreply.github.com>
2024-01-17 00:49:26 -0800

    Update scripts.js

--- docs/includes/scripts.js
+++ docs/includes/scripts.js
@@ -346,7 +346,7 @@ function getGames(packID) {
     type: 'GET',
     url: 'https://api.rawg.io/api/games?',
     data: {
-      key: '98188b8d3dd14a7f9edf6b2bf005a823',
+      key: process.env.API_KEY,
       search: search1
     }
     });
